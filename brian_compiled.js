bc7e5672168675d939dc6200ab31932d0bb26f2e
Alexey Kondratov <alex.lumir@gmail.com>
2014-08-10 13:18:30 +0400

    Reopen tabset on code change

--- brian_compiled.js
+++ brian_compiled.js
@@ -16,19 +16,25 @@ goog.require('lt.objs.editor');
 goog.require('lt.objs.editor.pool');
 goog.require('lt.objs.command');
 lt.plugins.brian.request = lt.util.load.node_module.call(null,"request");
+lt.plugins.brian.browser_opened = false;
 lt.plugins.brian.get_code = (function get_code(){var ed = lt.objs.find.current_ed.call(null);return new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"code","code",1016963423),lt.objs.editor.__GT_val.call(null,ed),new cljs.core.Keyword(null,"line","line",1017226086),cljs.core.get.call(null,lt.objs.editor.__GT_cursor.call(null,ed,"start"),new cljs.core.Keyword(null,"line","line",1017226086))], null);
 });
-lt.plugins.brian.get_data = (function get_data(url,callback){var code = lt.plugins.brian.get_code.call(null);return lt.plugins.brian.request.call(null,url,((function (code){
-return (function (e,r,body){return callback.call(null,[cljs.core.str("http://77.88.37.102:5000/static/figure.svg?line="),cljs.core.str(cljs.core.get.call(null,code,new cljs.core.Keyword(null,"line","line",1017226086))),cljs.core.str("&code="),cljs.core.str(encodeURIComponent(cljs.core.get.call(null,code,new cljs.core.Keyword(null,"code","code",1016963423))))].join('')).call(null);
-});})(code))
-);
-});
-lt.plugins.brian.render_data = (function render_data(path){lt.objs.command.exec_BANG_.call(null,new cljs.core.Keyword(null,"add-browser-tab","add-browser-tab",3663273910),path);
-return lt.objs.command.exec_BANG_.call(null,new cljs.core.Keyword(null,"tabset.prev","tabset.prev",1472322118));
-});
-lt.plugins.brian.brian_visualize = (function brian_visualize(str){lt.objs.command.exec_BANG_.call(null,new cljs.core.Keyword(null,"tabset.new","tabset.new",1444331601),"tabset");
+lt.plugins.brian.render_data = (function render_data(path){if(cljs.core.truth_(lt.plugins.brian.browser_opened))
+{lt.plugins.brian.close_btds.call(null);
+} else
+{}
+lt.objs.command.exec_BANG_.call(null,new cljs.core.Keyword(null,"tabset.new","tabset.new",1444331601));
 lt.objs.command.exec_BANG_.call(null,new cljs.core.Keyword(null,"tabset.next","tabset.next",1472250630));
-return lt.plugins.brian.get_data.call(null,"http://77.88.37.102:5000/static/figure.svg",lt.plugins.brian.render_data);
+lt.objs.command.exec_BANG_.call(null,new cljs.core.Keyword(null,"add-browser-tab","add-browser-tab",3663273910),path);
+lt.objs.command.exec_BANG_.call(null,new cljs.core.Keyword(null,"tabset.prev","tabset.prev",1472322118));
+lt.plugins.brian.browser_opened = true;
+});
+lt.plugins.brian.close_btds = (function close_btds(){lt.objs.command.exec_BANG_.call(null,new cljs.core.Keyword(null,"tabset.next","tabset.next",1472250630));
+lt.objs.command.exec_BANG_.call(null,new cljs.core.Keyword(null,"tabs.close","tabs.close",4150844154));
+lt.objs.command.exec_BANG_.call(null,new cljs.core.Keyword(null,"tabset.close","tabset.close",2327781609));
+lt.plugins.brian.browser_opened = false;
+});
+lt.plugins.brian.brian_visualize = (function brian_visualize(){var code = lt.plugins.brian.get_code.call(null);return lt.plugins.brian.render_data.call(null,[cljs.core.str("http://localhost:5000/eval?line="),cljs.core.str(cljs.core.get.call(null,code,new cljs.core.Keyword(null,"line","line",1017226086))),cljs.core.str("&code="),cljs.core.str(encodeURIComponent(cljs.core.get.call(null,code,new cljs.core.Keyword(null,"code","code",1016963423))))].join(''));
 });
 lt.objs.command.command.call(null,new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"command","command",1964298941),new cljs.core.Keyword(null,"brian-visualize","brian-visualize",1676687487),new cljs.core.Keyword(null,"desc","desc",1016984067),"Brian: visualize data",new cljs.core.Keyword(null,"exec","exec",1017031683),lt.plugins.brian.brian_visualize], null));
 }
--- src/lt/plugins/brian.cljs
+++ src/lt/plugins/brian.cljs
@@ -11,27 +11,45 @@
 ;; Define cljs binding to 'request' node module
 (def request (load/node-module "request"))
 
+(def browser-opened false)
+
 ;; Return current line and full code
 (defn get-code []
   (let [ed (lt.objs.find/current-ed)]
     { :code (editor/->val ed) :line (get (editor/->cursor ed "start") :line) }))
 
 ;; Fetch data and run render callback
-(defn get-data [url, callback]
-  (let [code (get-code)]
-    (request url (fn [e r body] ((callback (str "http://77.88.37.102:5000/static/figure.svg?line=" (get code :line) "&code=" (js/encodeURIComponent (get code :code)) )))))))
+;;(defn get-data [url, callback]
+;;  (let [code (get-code)]
+;;    (request url (fn [e r body] ((callback (str "http://localhost:5000/static/figure.svg?line=" (get code :line) "&code=" (js/encodeURIComponent (get code :code)) )))))))
 
 ;; Open browser to render data
 (defn render-data [path]
+  (if browser-opened
+    (close-btds))
+  (cmd/exec! :tabset.new)
+  (cmd/exec! :tabset.next)
   (cmd/exec! :add-browser-tab path)
-  (cmd/exec! :tabset.prev))
+  (cmd/exec! :tabset.prev)
+  (def browser-opened true))
+
+;; Close browser tabset
+(defn close-btds []
+  (cmd/exec! :tabset.next)
+  (cmd/exec! :tabs.close)
+  (cmd/exec! :tabset.close)
+  (def browser-opened false))
+
 
 ;; Main method to visualize data
-(defn brian-visualize [str]
-  (cmd/exec! :tabset.new "tabset")
-  (cmd/exec! :tabset.next)
-  (get-data "http://77.88.37.102:5000/static/figure.svg" render-data))
+;;(defn brian-visualize []
+;;    (cmd/exec! :tabset.new "tabset")
+;;    (get-data "http://localhost:5000/static/figure.svg" render-data))
 
+;; Main method to visualize data
+(defn brian-visualize []
+  (let [code (get-code)]
+      (render-data (str "http://localhost:5000/eval?line=" (get code :line) "&code=" (js/encodeURIComponent (get code :code)) ))))
 
 ;; Register global LT command
 (cmd/command {:command :brian-visualize
